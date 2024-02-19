fc5cd23c29e4a864aeba79d2b4501a5bcbd9652e
lailaniii <157762004+lailaniii@users.noreply.github.com>
2024-02-18 21:38:54 -0800

    Update Lab3_CF.cpp

--- lab 3 /Lab3_CF.cpp
+++ lab 3 /Lab3_CF.cpp
@@ -1,1 +1,11 @@
+int main()
+{
+    int x = 4;
+    int y = 5;
 
+    if (x > y){
+        printf("x is greater than y\n");
+    } else {
+        printf("x is less than or equal to y\n");
+    }
+}
