b275f357dee0eae1dd66a8c113e56ad5c1c1ebac
lailaniii <157762004+lailaniii@users.noreply.github.com>
2024-02-19 14:00:17 -0800

    Revert "Update Lab3_CF.cpp"
    This reverts commit fc5cd23c29e4a864aeba79d2b4501a5bcbd9652e.

--- lab 3 /Lab3_CF.cpp
+++ lab 3 /Lab3_CF.cpp
@@ -1,11 +1,1 @@
-int main()
-{
-    int x = 4;
-    int y = 5;
 
-    if (x > y){
-        printf("x is greater than y\n");
-    } else {
-        printf("x is less than or equal to y\n");
-    }
-}
