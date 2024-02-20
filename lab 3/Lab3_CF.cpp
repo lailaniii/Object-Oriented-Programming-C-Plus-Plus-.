c2178ef844fa13be3b09f76e4df4a344162416c0
lailaniii <157762004+lailaniii@users.noreply.github.com>
2024-02-19 22:25:23 -0800

    Update Lab3_CF.cpp

--- lab 3/Lab3_CF.cpp
+++ lab 3/Lab3_CF.cpp
@@ -1,1 +1,26 @@
+/***
+ * {lab 3 teaches us how to use boolean expressions, if/else statements, and loops}
+ ***/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    int x = 0;
+    int y = 5;
+    while(x < y) {
+        printf("%i\n",x);
+        x = x + 3;
+    }
+    if ( x % 2 == 0) {
+        printf("x is even\n");
+    } else {
+        printf(" x is odd\n");
+    }
+    return 0;
+}
+
+
 
